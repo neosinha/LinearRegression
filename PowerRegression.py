1389f9b46b84980c96730c0895e0cabf237f7f8d
Navendu Sinha <navendusinha@icloud.com>
2021-05-11 07:09:26 -0700

    Initial import

--- PowerRegression.py
+++ PowerRegression.py
@@ -4,6 +4,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+import statsmodels.api as sm
 
 # Training the Multiple Linear Regression model on the Training set
 from sklearn.linear_model import LinearRegression
@@ -15,14 +16,15 @@ from sklearn.preprocessing import OneHotEncoder
 from sklearn.metrics import r2_score, mean_squared_error
 
 # Importing the dataset
-dataset = pd.read_csv('data/bobblehead-01.csv' )
-print(dataset.describe())
+dataset = pd.read_excel('data/swactivity-0510.xlsx',
+                        engine='openpyxl')
 
+print(dataset.describe())
 #X = dataset.iloc[:,3:4 ].values #X variables, data is in col3 and 4
 #y = dataset.iloc[:,5].values #Y var
-
-xdata = dataset['AdSpend'].values
-ydata = dataset['Unit Sales'].values
+print(dataset.columns)
+ydata = dataset['USAGEW'].values
+xdata = dataset[['MORNING', 'AFTERNOON', 'EVENING']].values
 
 print("XVARS")
 print(xdata)    # prints X data
@@ -31,27 +33,28 @@ print(ydata)     # prints y Data
 # Encoding categorical data
 #ct = ColumnTransformer(transformers=[('encoder', OneHotEncoder(), [3,4])], remainder='passthrough')
 #X = np.array(ct.fit_transform(X))
-X = X.reshape(-1,2) # do array size mathching, 1x2 , 2 coz there are 2 variables
-y = y.reshape(-1,2) # do array size matching , 1x2
-print(X)
+#X = X.reshape(-1, 4)# do array size mathching, 1x2 , 2 coz there are 2 variables
+#y = y.reshape(-1,4) # do array size matching , 1x2
+#print(X)
 
 # Splitting the dataset into the Training set and Test set
 from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.1, random_state = 0)
+#X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.1, random_state = 0)
 scaler = MinMaxScaler()
 
-xdata = xdata.reshape(-1,1)
-ydata = ydata.reshape(-1,1)
+xdata = xdata.reshape(-1,4)
+ydata = ydata.reshape(-1,4)
 
 xscaled = scaler.fit_transform(xdata)
 yscaled = scaler.fit_transform(ydata)
 
+#model = sm.OLS(ydata, xdata).fit()
 regressor = LinearRegression() #instnatiate the Linear Regressor class
 #regressor = LogisticRegression()
 #regressor.fit(Xtrain, y_train) #fit the model using the complete data
-regressor.fit(xdata, ydata) #fit the model using the complete data
+regressor.fit(ydata, xdata) #fit the model using the complete data
 #regressor.fit(xscaled, yscaled) #fit the model using the complete data
-
+#print(model.summary())
 
 # Predicting the Test set results
 print("=====")
